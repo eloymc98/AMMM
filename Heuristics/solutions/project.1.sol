d20b7f055a22f1780a50ca3435b092b6a6b95bb7
Eloy Marin <eloymc16@gmail.com>
2020-12-13 11:12:56 +0100

    add plots

--- Heuristics/solutions/project.1.sol
+++ Heuristics/solutions/project.1.sol
@@ -1,75 +1,20 @@
-Solution found with cost 150.0
-Location 2 has a center of type 1. It serves 72.8 inhabitants, max is 73.
-Location 10 has a center of type 1. It serves 72.8 inhabitants, max is 73.
-Location 1 has a center of type 1. It serves 65.8 inhabitants, max is 73.
-Location 17 has a center of type 1. It serves 67 inhabitants, max is 73.
-Location 15 has a center of type 1. It serves 73 inhabitants, max is 73.
-Location 7 has a center of type 1. It serves 70 inhabitants, max is 73.
-Location 3 has a center of type 1. It serves 36 inhabitants, max is 73.
-Location 4 has a center of type 1. It serves 44 inhabitants, max is 73.
-Location 14 has a center of type 1. It serves 48 inhabitants, max is 73.
-Location 8 has a center of type 1. It serves 16 inhabitants, max is 73.
-City 0 primary center is at location 7 (distance 7.211102550927978, max is 10).
-City 0 secondary center is at location 10 (distance 6.324555320336759, max is 30).
-City 1 primary center is at location 17 (distance 8.0, max is 10).
-City 1 secondary center is at location 2 (distance 15.231546211727817, max is 30).
-City 2 primary center is at location 4 (distance 8.54400374531753, max is 10).
-City 2 secondary center is at location 2 (distance 4.123105625617661, max is 30).
-City 3 primary center is at location 2 (distance 5.830951894845301, max is 10).
-City 3 secondary center is at location 10 (distance 25.298221281347036, max is 30).
-City 4 primary center is at location 17 (distance 6.082762530298219, max is 10).
-City 4 secondary center is at location 1 (distance 3.1622776601683795, max is 30).
-City 5 primary center is at location 15 (distance 2.23606797749979, max is 10).
-City 5 secondary center is at location 1 (distance 7.0710678118654755, max is 30).
-City 6 primary center is at location 7 (distance 7.0710678118654755, max is 10).
-City 6 secondary center is at location 2 (distance 20.8806130178211, max is 30).
-City 7 primary center is at location 17 (distance 8.94427190999916, max is 10).
-City 7 secondary center is at location 2 (distance 14.142135623730951, max is 30).
-City 8 primary center is at location 15 (distance 1.4142135623730951, max is 10).
-City 8 secondary center is at location 1 (distance 9.0, max is 30).
-City 9 primary center is at location 10 (distance 4.242640687119285, max is 10).
-City 9 secondary center is at location 2 (distance 18.973665961010276, max is 30).
-City 10 primary center is at location 15 (distance 8.246211251235321, max is 10).
-City 10 secondary center is at location 2 (distance 11.40175425099138, max is 30).
-City 11 primary center is at location 10 (distance 6.4031242374328485, max is 10).
-City 11 secondary center is at location 2 (distance 17.11724276862369, max is 30).
-City 12 primary center is at location 14 (distance 10.0, max is 10).
-City 12 secondary center is at location 2 (distance 14.0, max is 30).
-City 13 primary center is at location 10 (distance 8.94427190999916, max is 10).
-City 13 secondary center is at location 2 (distance 17.72004514666935, max is 30).
-City 14 primary center is at location 10 (distance 8.06225774829855, max is 10).
-City 14 secondary center is at location 2 (distance 17.46424919657298, max is 30).
-City 15 primary center is at location 17 (distance 4.242640687119285, max is 10).
-City 15 secondary center is at location 2 (distance 12.727922061357855, max is 30).
-City 16 primary center is at location 17 (distance 7.0710678118654755, max is 10).
-City 16 secondary center is at location 2 (distance 13.92838827718412, max is 30).
-City 17 primary center is at location 17 (distance 8.06225774829855, max is 10).
-City 17 secondary center is at location 2 (distance 10.04987562112089, max is 30).
-City 18 primary center is at location 2 (distance 3.0, max is 10).
-City 18 secondary center is at location 10 (distance 24.186773244895647, max is 30).
-City 19 primary center is at location 3 (distance 9.219544457292887, max is 10).
-City 19 secondary center is at location 2 (distance 3.605551275463989, max is 30).
-City 20 primary center is at location 1 (distance 8.06225774829855, max is 10).
-City 20 secondary center is at location 2 (distance 7.0710678118654755, max is 30).
-City 21 primary center is at location 7 (distance 8.54400374531753, max is 10).
-City 21 secondary center is at location 2 (distance 18.35755975068582, max is 30).
-City 22 primary center is at location 14 (distance 9.055385138137417, max is 10).
-City 22 secondary center is at location 1 (distance 18.027756377319946, max is 30).
-City 23 primary center is at location 2 (distance 5.0990195135927845, max is 10).
-City 23 secondary center is at location 10 (distance 26.076809620810597, max is 30).
-City 24 primary center is at location 1 (distance 10.0, max is 10).
-City 24 secondary center is at location 2 (distance 9.848857801796104, max is 30).
-City 25 primary center is at location 2 (distance 2.0, max is 10).
-City 25 secondary center is at location 1 (distance 1.0, max is 30).
-City 26 primary center is at location 4 (distance 7.0710678118654755, max is 10).
-City 26 secondary center is at location 2 (distance 17.029386365926403, max is 30).
-City 27 primary center is at location 1 (distance 1.4142135623730951, max is 10).
-City 27 secondary center is at location 2 (distance 3.0, max is 30).
-City 28 primary center is at location 7 (distance 9.433981132056603, max is 10).
-City 28 secondary center is at location 2 (distance 12.529964086141668, max is 30).
-City 29 primary center is at location 10 (distance 7.615773105863909, max is 10).
-City 29 secondary center is at location 2 (distance 15.231546211727817, max is 30).
-City 30 primary center is at location 3 (distance 4.123105625617661, max is 10).
-City 30 secondary center is at location 2 (distance 11.704699910719626, max is 30).
-City 31 primary center is at location 8 (distance 8.54400374531753, max is 10).
-City 31 secondary center is at location 2 (distance 22.47220505424423, max is 30).
+Solution found with cost 105.0
+Location 0 has a center of type 2. It serves 5.0 inhabitants, max is 5.
+Location 2 has a center of type 1. It serves 10.3 inhabitants, max is 14.
+Location 3 has a center of type 1. It serves 10 inhabitants, max is 14.
+City 0 primary center is at location 3 (distance 1.4142135623730951, max is 4).
+City 0 secondary center is at location 0 (distance 2.23606797749979, max is 21).
+City 1 primary center is at location 0 (distance 0.0, max is 7).
+City 1 secondary center is at location 2 (distance 2.23606797749979, max is 12).
+City 2 primary center is at location 2 (distance 3.0, max is 4).
+City 2 secondary center is at location 0 (distance 2.8284271247461903, max is 21).
+City 3 primary center is at location 2 (distance 1.0, max is 4).
+City 3 secondary center is at location 0 (distance 1.4142135623730951, max is 21).
+City 4 primary center is at location 3 (distance 2.0, max is 4).
+City 4 secondary center is at location 0 (distance 1.0, max is 21).
+City 5 primary center is at location 3 (distance 1.0, max is 4).
+City 5 secondary center is at location 0 (distance 2.8284271247461903, max is 21).
+City 6 primary center is at location 2 (distance 3.605551275463989, max is 4).
+City 6 secondary center is at location 0 (distance 1.4142135623730951, max is 21).
+City 7 primary center is at location 3 (distance 2.8284271247461903, max is 4).
+City 7 secondary center is at location 0 (distance 1.0, max is 21).
---
+++ plot_results.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Creating a numpy array
+X = np.array([83.53, 0.60787606, 0.56355, 0.52685404, 161.22519922, 43, 160.5706632])
+Y = np.array([0, (180 - 172) / 172, (176 - 172) / 172, (176 - 172) / 172, 0, 0, 0])
+
+plt.xlabel('Time (seconds)')
+plt.ylabel('Obj. value difference (%)')
+plt.plot(X[0], Y[0], 'bs', color='blue', label='CPLEX', markersize=12)
+plt.plot(X[1], Y[1], 'ro', label='Greedy', color='red', markersize=12)
+plt.plot(X[2], Y[2], 'ro', label='Greedy + LS (First)', color='brown', markersize=12)
+plt.plot([X[3]], Y[3], 'ro', label='Greedy + LS (Best)', color='green', markersize=12)
+plt.plot(X[4], Y[4], 'ro', label='GRASP', color='black', markersize=12)
+plt.plot(X[5], Y[5], 'ro', label='GRASP + LS (First)', color='pink', markersize=12)
+plt.plot(X[6], Y[6], 'ro', label='GRASP + LS (Best)', color='violet', markersize=12)
+plt.legend()
+plt.savefig('plots/performance0.png')
+plt.show()
+#######################
+X = np.array([247.82, 6.1330142, 6.13849497, 4.53772783, 150.2052989, 242.9250381, 279.083453])
+Y = np.array([0, (183 - 157) / 157, (179 - 157) / 157, (179 - 157) / 157, (179 - 157) / 157, (192 - 157) / 157, (194 - 157) / 157])
+
+plt.xlabel('Time (seconds)')
+plt.ylabel('Obj. value difference (%)')
+plt.plot(X[0], Y[0], 'bs', color='blue', label='CPLEX', markersize=12)
+plt.plot(X[1], Y[1], 'ro', label='Greedy', color='red', markersize=12)
+plt.plot(X[2], Y[2], 'ro', label='Greedy + LS (First)', color='brown', markersize=12)
+plt.plot([X[3]], Y[3], 'ro', label='Greedy + LS (Best)', color='green', markersize=12)
+plt.plot(X[4], Y[4], 'ro', label='GRASP', color='black', markersize=12)
+plt.plot(X[5], Y[5], 'ro', label='GRASP + LS (First)', color='pink', markersize=12)
+plt.plot(X[6], Y[6], 'ro', label='GRASP + LS (Best)', color='violet', markersize=12)
+plt.legend()
+plt.savefig('plots/performance1.png')
+plt.show()
+
+########################
+X = np.array([344.53, 0.72352099, 0.75220394, 0.77671599, 7.54188013, 2.10909414, 1.93550706])
+Y = np.array([0, (339 - 272) / 272, (339 - 272) / 272, (317 - 272) / 272, (292 - 272) / 272, (292 - 272) / 272, (292 - 272) / 272])
+
+plt.xlabel('Time (seconds)')
+plt.ylabel('Obj. value difference (%)')
+plt.plot(X[0], Y[0], 'bs', color='blue', label='CPLEX', markersize=12)
+plt.plot(X[1], Y[1], 'ro', label='Greedy', color='red', markersize=12)
+plt.plot(X[2], Y[2], 'ro', label='Greedy + LS (First)', color='brown', markersize=12)
+plt.plot([X[3]], Y[3], 'ro', label='Greedy + LS (Best)', color='green', markersize=12)
+plt.plot(X[4], Y[4], 'ro', label='GRASP', color='black', markersize=12)
+plt.plot(X[5], Y[5], 'ro', label='GRASP + LS (First)', color='pink', markersize=12)
+plt.plot(X[6], Y[6], 'ro', label='GRASP + LS (Best)', color='violet', markersize=12)
+plt.legend()
+plt.savefig('plots/performance2.png')
+plt.show()
+
+########################
+X = np.array([822.54, 16.2120938, 14.08670282, 17.13206673, 81.23526192, 21.24188709, 21.86853886])
+Y = np.array([0, (502 - 335) / 335, (346 - 335) / 335, (346 - 335) / 335, (453 - 335) / 335, (346 - 335) / 335, (335 - 335) / 335])
+
+plt.xlabel('Time (seconds)')
+plt.ylabel('Obj. value difference (%)')
+plt.plot(X[0], Y[0], 'bs', color='blue', label='CPLEX', markersize=12)
+plt.plot(X[1], Y[1], 'ro', label='Greedy', color='red', markersize=12)
+plt.plot(X[2], Y[2], 'ro', label='Greedy + LS (First)', color='brown', markersize=12)
+plt.plot([X[3]], Y[3], 'ro', label='Greedy + LS (Best)', color='green', markersize=12)
+plt.plot(X[4], Y[4], 'ro', label='GRASP', color='black', markersize=12)
+plt.plot(X[5], Y[5], 'ro', label='GRASP + LS (First)', color='pink', markersize=12)
+plt.plot(X[6], Y[6], 'ro', label='GRASP + LS (Best)', color='violet', markersize=12)
+plt.legend()
+plt.savefig('plots/performance3.png')
+plt.show()
+
+########################
+X = np.array([1354, 15.9951348, 15.43929505, 22.12564182, 40.32106233, 115.57565212, 165.2543807])
+Y = np.array([0, (163 - 160) / 160, (163 - 160) / 160, (163 - 160) / 160, (174 - 160) / 160, (163 - 160) / 160, (163 - 160) / 160])
+
+plt.xlabel('Time (seconds)')
+plt.ylabel('Obj. value difference (%)')
+plt.plot(X[0], Y[0], 'bs', color='blue', label='CPLEX', markersize=12)
+plt.plot(X[1], Y[1], 'ro', label='Greedy', color='red', markersize=12)
+plt.plot(X[2], Y[2], 'ro', label='Greedy + LS (First)', color='brown', markersize=12)
+plt.plot([X[3]], Y[3], 'ro', label='Greedy + LS (Best)', color='green', markersize=12)
+plt.plot(X[4], Y[4], 'ro', label='GRASP', color='black', markersize=12)
+plt.plot(X[5], Y[5], 'ro', label='GRASP + LS (First)', color='pink', markersize=12)
+plt.plot(X[6], Y[6], 'ro', label='GRASP + LS (Best)', color='violet', markersize=12)
+plt.legend()
+plt.savefig('plots/performance4.png')
+plt.show()
